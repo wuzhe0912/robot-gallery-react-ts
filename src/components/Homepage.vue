4fa8c6bdbfd7a3aa3cca68533bcb7e9a70f96439
zhethon <kgb00128@gmail.com>
2018-08-02 07:52:01 +0800

    d3 with vue demo

---
+++ src/components/Homepage.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+
+</template>
+
+<script>
+import * as d3 from 'd3'
+
+export default {
+  name: 'Homepage',
+  data () {
+    return {
+    }
+  },
+  created () {
+    this.createSvg()
+  },
+  methods: {
+    createSvg () {
+      d3.select('body').append('div').text('d3')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+</style>
